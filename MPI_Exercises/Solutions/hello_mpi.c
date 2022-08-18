628024ed142c21516693e8f038188375e5ff1bc2
tgmattso <timothy.g.mattson@intel.com>
2022-08-18 15:38:37 -0700

    Added lectures

--- MPI_Exercises/Solutions/hello_mpi.c
+++ MPI_Exercises/Solutions/hello_mpi.c
@@ -3,11 +3,17 @@
 int main (int argc, char **argv)
 {
    int i, ID, Nprocs;
+   char name[MPI_MAX_PROCESSOR_NAME];
+   int namLen;
 
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &ID);
    MPI_Comm_size(MPI_COMM_WORLD, &Nprocs);
    printf(" hello from %d of %d\n",ID, Nprocs);
+   
+   MPI_Get_processor_name(name,&namLen);
+   printf("%s  %d\n",name,namLen);
+   
    MPI_Finalize();
 }	  
 
