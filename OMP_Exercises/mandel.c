133d1436daa547d75d6c75a70e9cfa9060c1aee1
tgmattso <timothy.g.mattson@intel.com>
2023-07-19 08:41:34 -0700

    removed default shared

--- OMP_Exercises/mandel.c
+++ OMP_Exercises/mandel.c
@@ -38,7 +38,7 @@ int main(){
 //   Loop over grid of points in the complex plane which contains the Mandelbrot set,
 //   testing each point to see whether it is inside or outside the set.
 
-#pragma omp parallel for default(shared) private(c,eps)
+#pragma omp parallel for private(c,eps)
    for (i=0; i<NPOINTS; i++) {
      for (j=0; j<NPOINTS; j++) {
        c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
