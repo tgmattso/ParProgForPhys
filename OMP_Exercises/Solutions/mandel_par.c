0191b63c5b3f7005b67d347d845735a1cef39ee1
tgmattso <timothy.g.mattson@intel.com>
2023-07-26 19:00:22 -0700

    updated mandel

--- OMP_Exercises/Solutions/mandel_par.c
+++ OMP_Exercises/Solutions/mandel_par.c
@@ -8,7 +8,7 @@
 **  USAGE:   Program runs without input ... just run the executable
 **            
 **  HISTORY: Written:  (Mark Bull, August 2011).
-**           Changed "complex" to "d_complex" to avoid collsion with 
+**           Changed "comples" to "d_comples" to avoid collsion with 
 **           math.h complex type (Tim Mattson, September 2011)
 */
 
@@ -22,31 +22,23 @@
 
 void testpoint(double, double);
 
-// Mandelbrot set computed through an iterative process 
-//
-//    z_n = z_(n-1) squared plus C 
-//
-// z and C are complex.  Start with z_0 = 0.  The set occupies the 
-// complex plane where each (x,y) point is (Creal,Cimaginary)
-
-
 int numoutside = 0;
 
 int main(){
    int i, j;
    double area, error, eps  = 1.0e-5;
-   double Creal;
-   double Cimag;
+   double cimag, creal;
+
 
 //   Loop over grid of points in the complex plane which contains the Mandelbrot set,
 //   testing each point to see whether it is inside or outside the set.
 
-#pragma omp parallel for private(Creal,Cimag,j)
+#pragma omp parallel for private(creal, cimag, j) firstprivate(eps)
    for (i=0; i<NPOINTS; i++) {
      for (j=0; j<NPOINTS; j++) {
-       Creal = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-       Cimag = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-       testpoint(Creal, Cimag);
+       creal = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
+       cimag = 1.125*(double)(j)/(double)(NPOINTS)+eps;
+       testpoint(creal, cimag);
      }
    }
 
@@ -60,22 +52,20 @@ area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS
 
 }
 
-void testpoint(double Creal, double Cimag){
+void testpoint(double creal, double cimag){
 
-// Does the iteration z=z*z+c, until |z| > 2 when point is known to be outside set
+// iterate z=z*z+c, until |z| > 2 when point is known to be outside set
 // If loop count reaches MAXITER, point is considered to be inside the set
 
-       double zr, zi; 
+       double zreal, zimag, temp;
        int iter;
-       double temp;
+       zreal = creal;  zimag = cimag;
 
-       zr = Creal;     
-       zi = Cimag;
        for (iter=0; iter<MAXITER; iter++){
-         temp = (zr*zr)-(zi*zi)+Creal;
-         zi = zr*zi*2+Cimag;
-         zr = temp;
-         if ((zr*zr+zi*zi)>4.0) {
+         temp = (zreal*zreal)-(zimag*zimag)+creal;
+         zimag = zreal*zimag*2+cimag;
+         zreal = temp;
+         if ((zreal*zreal+zimag*zimag)>4.0) {
            #pragma omp critical
               numoutside++;
            break;
